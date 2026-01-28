af535b258620b2d103144fc689eb95bb0efa6556
almera <a.mentok@primemanpower.jp>
2026-01-28 10:50:17 +0800

    2.13: The Phonebook step 8

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
@@ -10,7 +9,17 @@ const create = (newPerson) => {
   return axios.post(baseUrl, newPerson)
 }
 
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+const update = (id, updatedPerson) => {
+  return axios.put(`${baseUrl}/${id}`, updatedPerson)
+}
+
 export default {
   getAll,
-  create
+  create,
+  remove,
+  update
 }
